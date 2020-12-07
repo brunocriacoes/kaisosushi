9006ca8f889037be26915631461fc237a26feac9
Sanppy <iagodamotaalves@outlook.com>
2020-12-07 11:17:39 -0300

    Mudanças no index, incluindo plano de fundo e alerta de e-mail e senha

--- view/admin/header.php
+++ view/admin/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="<?php echo dir_template( '/view/admin/img/maki.png' ); ?>">
+    <link rel="icon" href="<?php echo dir_template( '/view/admin/img/ico.svg' ); ?>">
     <link rel="stylesheet" href="<?php echo dir_template( '/view/admin/style.css' ); ?>">
     <title><?php echo get_title_site(); ?></title>
 </head>
--- view/admin/index.php
+++ view/admin/index.php
@@ -3,16 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="<?php echo dir_template( '/view/admin/img/maki.png' ); ?>">
+    <link rel="icon" href="<?php echo dir_template( '/view/admin/img/ico.png' ); ?>">
     <link rel="stylesheet" href="<?php echo dir_template( '/view/admin/style.css' ); ?>">
     <title><?php echo get_title_site(); ?></title>
 </head>
 <body class="estoylokito">
     <section id="index">
-        <img src="<?php echo dir_template( '/view/admin/img/logo.png' ); ?>" alt="">
-        <form action="<?php echo dir_template( '/admin/painel' ); ?>">
-            <input type="email" placeholder="E-mail">
-            <input type="password" placeholder="Password">
+        <img src="<?php echo dir_template( '/view/admin/img/kaiso.svg' ); ?>" alt="">
+        <div class="alerta">
+            E-mail ou Senha incorretos. Insira novamente! <span title="Fechar">X</span>
+        </div>
+        <form action="<?php echo dir_template( '/admin/painel' ); ?>" method="POST">
+            <input name="email" require type="email" placeholder="E-mail">
+            <input name="pass" require type="password" placeholder="Password">
             <div>
                 <input type="submit" value="Entrar"> <a href="rec-senha.html">Não me lembro da minha password</a>
             </div>
