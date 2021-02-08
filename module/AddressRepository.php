ad67142c2085bdd82a2f4699159b12b24eb5421a
brunocriacoes <br.rafael@outlook.com>
2021-02-08 09:18:30 -0300

    fix

--- module/AddressRepository.php
+++ module/AddressRepository.php
@@ -3,10 +3,16 @@ class AddressRepository
 {
     function register( array $params )
     {
-        query( "INSERT INTO address 
-        ( client_id, name, address, number, city, post_code, complement ) 
-        VALUES
-        ( {$params['client_id']}, '{$params['name']}', '{$params['address']}', '{$params['number']}', '{$params['city']}', '{$params['post_code']}', '{$params['complement']}' ) " );
+        $exist_post_zip = $this->exist( $params['client_id'], $params['post_code'] );        
+        if( isset($exist_post_zip[0]['id']) ) {
+            $params['id'] = $exist_post_zip[0]['id'];
+            $this->update($params);
+        } else {
+            query( "INSERT INTO address 
+            ( client_id, name, address, number, city, post_code, complement ) 
+            VALUES
+            ( {$params['client_id']}, '{$params['name']}', '{$params['address']}', '{$params['number']}', '{$params['city']}', '{$params['post_code']}', '{$params['complement']}' ) " );
+        }
     }    
     function update( array $params )
     {
@@ -23,6 +29,12 @@ class AddressRepository
     function about( int $id )
     {
         return query( "SELECT * FROM address WHERE id=$id" );
+    }
+    function exist( $client_id, $post_code )
+    {
+        $sql = "SELECT * FROM address WHERE post_code='{$post_code}' AND client_id='{$client_id}' ";
+        $query = query($sql);
+        return $query;
     }
 
 }
